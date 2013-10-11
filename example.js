6513be885e1e81be7e0a04c84019e54413f4630a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-10-11 09:22:33 +0100

    Updated example

--- example.js
+++ example.js
@@ -1,12 +1,21 @@
+var http = require('http');
 var request = require('./caching');
+var LRU = require('lru-cache');
+var store = new request.MemoryStore(new LRU());
+var cache = new request.Cache(store);
 
-var cache = new request.MemoryCache();
+http.createServer(function(req, res) {
+  var date = new Date().toUTCString();
+  res.writeHead(200, { 'Date': date, 'Cache-Control': 'max-age=5' });
+  console.log("Server hit!");
+  res.end('Hello');
+}).listen(3000, function(err) {
 
-request('http://localhost:3000', { cache: cache }, function(err, res, body) {
-    console.log(body);
-    setTimeout(function() { 
-      request('http://localhost:3000', { cache: cache }, function(err, res, body) {
-        console.log(body);
-      });
-    }, 2000);
+  setInterval(function() { 
+    request('http://localhost:3000', { cache: cache }, function(err, res, body) {
+      console.log("Client: " + body);
+    });
+  }, 2000);
+
 });
+
