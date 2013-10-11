1eb5f766deaff5db3c394c730bf5d2f1d0ca8e62
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-10-11 09:44:35 +0100

    rename variables for clarity

--- caching.js
+++ caching.js
@@ -1,11 +1,11 @@
 var request = require('request');
 
-var null_cache = {
+var noOpCache = {
   get: function(uri, cb) {
     cb();
   },
 
-  set: function(uri, private, value, expires_millis, cb) {
+  set: function(uri, private, value, ttlMillis, cb) {
     cb();
   }
 };
@@ -14,12 +14,12 @@ module.exports = function(uri, options, callback) {
   if (typeof uri === 'undefined') throw new Error('undefined is not a valid uri or options object.');
   if (typeof options === 'function' && !callback) callback = options;
 
-  var cache = options.cache || null_cache;
+  var cache = options.cache || noOpCache;
 
   cache.get(uri, function(err, value) {
     if (err) return callback(err);
 
-    if (value && new Date().getTime() <= value.expires_millis) {
+    if (value && new Date().getTime() <= value.expiryTimeMillis) {
       return callback(null, value.response, value.response.body);
     }
 
@@ -39,8 +39,8 @@ module.exports = function(uri, options, callback) {
         return callback(null, value.response, value.response.body);
       }
 
-      var cacheable = false;
-      var expires_millis = undefined;
+      var isCacheable = false;
+      var expiryTimeMillis = undefined;
       var private = false;
       if ('cache-control' in res.headers) {
         // In case of Cache-Control: no-cache, cacheable should remain false.
@@ -53,26 +53,26 @@ module.exports = function(uri, options, callback) {
         });
         private = cacheControl.private;
         if (cacheControl['max-age']) {
-          cacheable = true;
+          isCacheable = true;
           var date = new Date(res.headers['date']);
           var seconds = +cacheControl['max-age'];
-          expires_millis = date.getTime() + 1000*seconds;
+          expiryTimeMillis = date.getTime() + 1000*seconds;
         }
       } else if ('expires' in res.headers) {
-        cacheable = true;
+        isCacheable = true;
         var expires = new Date(res.headers['expires']);
-        expires_millis = expires.getTime();
+        expiryTimeMillis = expires.getTime();
       } else if ('etag' in res.headers) {
-        cacheable = true;
+        isCacheable = true;
       } else if ('last-modified' in res.headers) {
-        cacheable = true;
+        isCacheable = true;
       }
 
-      if(cacheable) {
-        var ttl_millis = Math.max(expires_millis - new Date().getTime(), 0);
-        var cachedResponse = { response: cachable(res), expires_millis: expires_millis };
+      if(isCacheable) {
+        var ttlMillis = Math.max(expiryTimeMillis - new Date().getTime(), 0);
+        var cachedResponse = { response: cachedResponseProperties(res), expiryTimeMillis: expiryTimeMillis };
 
-        cache.set(uri, private, cachedResponse, ttl_millis, function(err) {
+        cache.set(uri, private, cachedResponse, ttlMillis, function(err) {
           callback(err, res, body);
         });
       } else {
@@ -81,7 +81,7 @@ module.exports = function(uri, options, callback) {
     });
   });
 
-  function cachable(res) {
+  function cachedResponseProperties(res) {
     return {
       statusCode: res.statusCode,
       headers: res.headers,
@@ -90,10 +90,10 @@ module.exports = function(uri, options, callback) {
   }
 };
 
+// Copy over the original request API
 for(var func in request) {
   module.exports[func] = request[func];
 }
-
 module.exports.get = module.exports;
 
 module.exports.Cache = require('./lib/cache');
