b829cabeff34207e8426fc8c926acc5e5d2c0fae
Jan Dolezel <jando@salsitasoft.com>
2015-01-23 16:52:57 +0100

    Better error handling

--- caching.js
+++ caching.js
@@ -37,7 +37,7 @@ module.exports = function(uri, options, callback) {
 
       if(res.statusCode == 304) {
         if (!value) { // weird stuff ???
-          throw new Error('value is unexpectedly null for "' + uri + '"')
+          return callback('value is unexpectedly null for "' + uri + '"');
         } else {
           value.response.headers = res.headers;
           return callback(null, value.response, value.response.body);
